1958687eb123bf494798078c7c4f0ef913b4b6f7
fangkun <panzhangyi@squrab.com>
2020-05-12 13:06:38 +0800

    1

--- src/com/kframe/java/TestHashMap.java
+++ src/com/kframe/java/TestHashMap.java
@@ -5,6 +5,27 @@ import java.util.HashMap;
 public class TestHashMap {
 	public static void main(String[] args) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		System.out.println(map.put("sasa", 1));
 		map.put("sasa", 1);
+		map.put("sasa1", 1);
+		map.put("sasa2", 1);
+		map.put("sasa3", 1);
+		map.put("sasa4", 1);
+		map.put("sasa5", 1);
+		map.put("sasa6", 1);
+		map.put("sasa7", 1);
+		map.put("sasa8", 1);
+		map.put("sasa9", 1);
+		map.put("sasa10", 1);
+		map.put("sasa11", 1);
+		map.put("sasa12", 1);
+		map.put("sasa13", 1);
+		map.put("sasa14", 1);
+		map.put("sasa15", 1);
+		map.put("sasa16", 1);
+		map.put("sasa17", 1);
+		map.put("sasa17", 1);
+		System.out.println(map.get("sasa"));
+		map.put("sasa17", 1);
 	}
 }
---
+++ src/com/kframe/juc/executorpoll/ScheduledThreadPoolExecutorTest.java
@@ -0,0 +1,9 @@
+package com.kframe.juc.executorpoll;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+public class ScheduledThreadPoolExecutorTest {
+	public static void main(String[] args) {
+		ScheduledThreadPoolExecutor executor;
+	}
+}
